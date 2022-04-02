1d8e4b15a031299f3dfd94109d11d48d30f86804
walterunderwood <walonsounderwood@gmail.com>
2022-04-02 11:32:22 -0700

    Added btnClicked Event on JavaScript

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,5 @@
-
+function btnClicked() {
+  document.getElementById("container").style.display = "block";
+  document.getElementsById("startBtn").style.display = "flex";
+  console.log("return");
+}
