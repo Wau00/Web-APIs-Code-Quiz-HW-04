368ed2fed0b21f305649494e2bf6ec504d1fe895
walterunderwood <walonsounderwood@gmail.com>
2022-04-04 21:49:36 -0700

    Setting questions, choices and answers variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,50 @@
 var btnTimer;
 var timer;
 
+var data = [
+  {
+    question:
+      "Which of the following variable is everywhere in your JavaScript code?",
+    choices: [
+      "global variable",
+      "local variable",
+      "Both of the above",
+      "None of the above",
+    ],
+    answer: "global variable",
+  },
+  {
+    question: "How to write an if statment in JavaScript?",
+    choices: ["if i === 4", "if (i === 5)", "if [i === 5]", "else (if === 5)"],
+    answer: "if (i === 5)",
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<button>", "<div>", "<script>", "<html>"],
+    answer: "<script>",
+  },
+  {
+    question: "How do you write 'Hello World' in alert box?",
+    choices: [
+      "click('Hello World')",
+      "console.log('Hello World')",
+      "alert('Hello World')",
+      "None of the above",
+    ],
+    answer: "alert('Hello World')",
+  },
+  {
+    question: "What is negative Infinity?",
+    choices: ["A number", "A tag", "A function", "All of the above"],
+    answer: "A number",
+  },
+  {
+    question: "When was JavaScript created?",
+    choices: ["1981", "1999", "2006", "1995"],
+    answer: "1995",
+  },
+];
+
 const start = document.getElementById("startHide");
 start.addEventListener("click", startGame);
 
@@ -23,3 +67,5 @@ function startTimer() {
     }
   }, 1000);
 }
+
+function questionsTrivia() {}
