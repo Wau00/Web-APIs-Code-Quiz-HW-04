67f55e883de382efa0ae4271e32958bf13b7ce65
walterunderwood <walonsounderwood@gmail.com>
2022-04-04 21:08:48 -0700

    Setting and creating temporizer on <start> button; setInterval(), clearInterval()

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,25 @@
-function btnClicked() {
+var btnTimer;
+var timer;
+
+const start = document.getElementById("startHide");
+start.addEventListener("click", startGame);
+
+function startGame() {
+  btnTimer = 60;
   document.getElementById("container").style.display = "block";
-  document.getElementsById("startBtn").style.display = "flex";
-  console.log("return");
+  startTimer();
+}
+function startTimer() {
+  timer = setInterval(function () {
+    btnTimer--;
+    document.querySelector(".temporizer").textContent = btnTimer;
+    if (btnTimer >= 0) {
+      if (btnTimer <= 0) {
+        clearInterval(timer);
+      }
+    }
+    if (btnTimer === 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
