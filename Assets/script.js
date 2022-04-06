582470f22b581e2617ca313a470a1b87de3be29c
walterunderwood <walonsounderwood@gmail.com>
2022-04-05 23:48:07 -0700

    Creating function that add and rest points and seconds out of the timer and finalscore

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 var btnTimer;
 var timer;
-var finalscore;
+var finalScore;
 
 let scoreAlert = document.getElementById("scoreAlert")
 
@@ -11,16 +11,19 @@ let questionFour = document.getElementById("fourthQuestion");
 let questionFive = document.getElementById("fifthQuestion");
 let enterInitials = document.getElementById("finalResult");
 
-
 const start = document.getElementById("startHide");
 start.addEventListener("click", startGame);
 
 function startGame() {
   btnTimer = 60;
+  finalScore = 100;
   document.getElementById("firstQuestion").style.display = "block";
   startTimer();
   }
 
+
+  
+ 
 
 function startTimer() {
   timer = setInterval(function () {
@@ -32,46 +35,78 @@ function startTimer() {
         clearInterval(timer);
       }
     }
-    if (btnTimer === 0) {
-      clearInterval(timer);
-    }
+    
   }, 1000);
 }
 
+
+function wrongAnswer(){ 
+  
+  scoreAlert.textContent = "Incorrect! -25pts and -10s"
+  scoreAlert.style.color = "red";
+  scoreAlert.style.display = "block";
+  
+  btnTimer= btnTimer- 10;
+  if (btnTimer < 0){
+   clearInterval(timer);
+  }
+  document.getElementById("finalscore").textContent = finalScore;
+  finalScore = finalScore-25;
+  console.log (finalScore);
+
+}
 
 function correctAnswer(){
     questionOne.style.display = "none";
     questionTwo.style.display = "block";
     scoreAlert.style.display = "block";
+    scoreAlert.textContent = "Correct! +5pts and +5s"
+    scoreAlert.style.color = "green";
+    btnTimer= btnTimer+5;
+    finalScore= finalScore +5;
 }
 
 function secondcorrectAnswer (){
 questionTwo.style.display = "none";
 questionThree.style.display = "block";
 scoreAlert.style.display = "block";
-
+    scoreAlert.textContent = "Correct! +5pts and +5s"
+    scoreAlert.style.color = "green";
+    btnTimer= btnTimer+5;
+    finalScore= finalScore +5;
+    console.log (finalScore);
 }
 
 function thirdcorrectAnswer (){
   questionThree.style.display = "none";
   questionFour.style.display = "block";
   scoreAlert.style.display = "block";
+  scoreAlert.textContent = "Correct! +5pts + 5s"
+  scoreAlert.style.color = "green";
+  btnTimer= btnTimer+5;
+  finalScore= finalScore +5;
+  console.log (finalScore);
 }
 
 function fourthcorrectAnswer (){
   questionFour.style.display = "none";
   questionFive.style.display = "block";
   scoreAlert.style.display = "block";
+  scoreAlert.textContent = "Correct! +5pts + 5s"
+  scoreAlert.style.color = "green";
+  btnTimer= btnTimer+5;
+  finalScore= finalScore +5;
+  console.log (finalScore);
 }
 
 function fifthcorrectAnswer (){
   questionFive.style.display = "none";
   enterInitials.style.display = "block";
   scoreAlert.style.display = "block";
-}
-
-function wrongAnswer(){ 
-  scoreAlert.textContent = "Incorrect! -25 pts and -10s"
-  scoreAlert.style.color = "red";
-  scoreAlert.style.display = "block";
+  scoreAlert.textContent = "Correct! +5pts + 5s"
+  scoreAlert.style.color = "green";
+  scoreAlert.style.display = "none";
+  btnTimer= btnTimer+5;
+  finalScore= finalScore +5;
+  console.log (finalScore);
 }
