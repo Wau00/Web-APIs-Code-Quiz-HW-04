2fbc16b0feb0b20b6d3d399bd6167f3b826e23c0
walterunderwood <walonsounderwood@gmail.com>
2022-04-05 17:21:53 -0700

    Creatinf final score Window after the player answer the question

--- Assets/script.js
+++ Assets/script.js
@@ -10,49 +10,6 @@ var btnSelector = document.getElementsByClassName("answerBtn");
 
 
 
-var dataInfo = [
-  {
-    promptQuest:
-      "Which of the following variable is everywhere in your JavaScript code?",
-    choices: [
-      "global variable",
-      "local variable",
-      "Both of the above",
-      "None of the above",
-    ],
-    answer: "global variable",
-  },
-  {
-    promptQuest: "How to write an if statment in JavaScript?",
-    choices: ["if i === 4", "if (i === 5)", "if [i === 5]", "else (if === 5)"],
-    answer: "if (i === 5)",
-  },
-  {
-    promptQuest: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<button>", "<div>", "<script>", "<html>"],
-    answer: "<script>",
-  },
-  {
-    promptQuest: "How do you write 'Hello World' in alert box?",
-    choices: [
-      "click('Hello World')",
-      "console.log('Hello World')",
-      "alert('Hello World')",
-      "None of the above",
-    ],
-    answer: "alert('Hello World')",
-  },
-  {
-    promptQuest: "What is negative Infinity?",
-    choices: ["A number", "A tag", "A function", "All of the above"],
-    answer: "A number",
-  },
-  {
-    promptQuest: "When was JavaScript created?",
-    choices: ["1981", "1999", "2006", "1995"],
-    answer: "1995",
-  },
-];
 
 const start = document.getElementById("startHide");
 start.addEventListener("click", startGame);
@@ -64,6 +21,7 @@ function startGame() {
 }
 function startTimer() {
   timer = setInterval(function () {
+
     btnTimer--;
     document.querySelector(".temporizer").textContent = btnTimer;
     if (btnTimer >= 0) {
@@ -99,3 +57,7 @@ function fourthcorrectAnswer (){
   document.getElementById("fifthQuestion").style.display = "block";
 }
 
+function fifthcorrectAnswer (){
+  document.getElementById("fifthQuestion").style.display = "none";
+  document.getElementById("finalResult").style.display = "block";
+}
